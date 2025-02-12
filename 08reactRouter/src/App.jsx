24eb7e9e0c14dbc7e95ba2f4f134268c3d5b98cd
Pawan Kumar <cnct.pawan@gmail.com>
2025-02-12 15:17:37 +0530

    Started a new project on router

---
+++ 08reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-red-600 p-4 text-white'>Pawan</h1>
+    </>
+  )
+}
+
+export default App
